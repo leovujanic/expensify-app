c3b70ddcdbe780532821ecbe854a0e2a0645b730
Leo Vujanic <leonard.vujanic@gmail.com>
2018-11-10 11:49:34 +0100

    Update formatting for amount and time

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import connect from "react-redux/es/connect/connect";
 import {Link} from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 
 export const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
@@ -8,7 +10,8 @@ export const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) 
 		<Link to={`/edit/${id}`}>
 			<h2>{description}</h2>
 		</Link>
-		<p>{amount} - {createdAt}</p>
+		<p>
+			{numeral(amount).format('$0,0.00')} - {moment(createdAt).format('MMMM Do, YYYY')}</p>
 	</div>
 );
 
