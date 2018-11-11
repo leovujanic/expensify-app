39fe5fd40e639a972efb0070b5d8e4cbbc1d5cf3
Leo Vujanic <leonard.vujanic@gmail.com>
2018-11-11 23:00:18 +0100

    Add loading page

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css';
 import {firebase} from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 
 const store = configureStore();
@@ -28,7 +29,7 @@ const renderApp = () => {
 };
 
 
-ReactDOM.render(<p>Loading...</p>, appRoot);
+ReactDOM.render(<LoadingPage/>, appRoot);
 
 firebase.auth().onAuthStateChanged((user) => {
 	if (user) {
