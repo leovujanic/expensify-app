63aab025e9dadab0ffca9a659f03439f9b019608
Leo Vujanic <leonard.vujanic@gmail.com>
2018-11-11 18:16:01 +0100

    Refactor AppRouter to use public and private routes

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -8,20 +8,21 @@ import HelpPage from './../components/HelpPage';
 import NotFoundPage from './../components/NotFoundPage';
 import Header from './../components/Header';
 import LoginPage from './../components/LoginPage';
+import PrivateRoute from './PrivateRoute';
+import PublicRoute from './PublicRoute';
 
 export const history = createHistory();
 
 const AppRouter = () => (
 	<Router history={history}>
 		<div>
-			<Header/>
 			<Switch>
-				<Route path="/" component={LoginPage} exact={true}/>
-				<Route path="/dashboard" component={ExpenseDashboardPage}/>
-				<Route path="/create" component={AddExpensePage}/>
-				<Route path="/edit/:id" component={EditExpensePage}/>
-				<Route path="/help" component={HelpPage}/>
-				<Route component={NotFoundPage}/>
+				<PublicRoute path="/" component={LoginPage} exact={true}/>
+				<PrivateRoute path="/dashboard" component={ExpenseDashboardPage}/>
+				<PrivateRoute path="/create" component={AddExpensePage}/>
+				<PrivateRoute path="/edit/:id" component={EditExpensePage}/>
+				<PrivateRoute path="/help" component={HelpPage}/>
+				<PrivateRoute component={NotFoundPage}/>
 			</Switch>
 		</div>
 	</Router>
