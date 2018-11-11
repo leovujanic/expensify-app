97f0fa3cfe5968bbbdce60b60bd3e6eb2e65ef24
Leo Vujanic <leonard.vujanic@gmail.com>
2018-11-11 17:30:43 +0100

    Small refactor - add on AuthStateChange redirection

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import {Provider} from 'react-redux';
 import AppRouter, {history} from "./routers/AppRouter"
 import configureStore from './store/configureStore';
 import {startSetExpenses} from './actions/expenses'
-import {setTextFilter} from './actions/filters'
 import 'normalize.css/normalize.css'
 import './styles/styles.scss'
 import 'react-dates/lib/css/_datepicker.css';
@@ -12,27 +11,36 @@ import {firebase} from './firebase/firebase';
 
 
 const store = configureStore();
-
 const appRoot = document.getElementById('app');
-
 const jsx = (
 	<Provider store={store}>
 		<AppRouter/>
 	</Provider>
 );
 
-store.dispatch(startSetExpenses()).then(() => {
-	ReactDOM.render(
-		jsx,
-		appRoot
-	);
-});
+let hasRendered = false;
+
+const renderApp = () => {
+	if (!hasRendered) {
+		ReactDOM.render(jsx, appRoot);
+	}
+};
+
+
+ReactDOM.render(<p>Loading...</p>, appRoot);
 
 firebase.auth().onAuthStateChanged((user) => {
 	if (user) {
 		console.log('log in');
+		store.dispatch(startSetExpenses()).then(() => {
+			renderApp();
+			if (history.location.pathname === '/') {
+				history.push('/dashboard');
+			}
+		});
 	} else {
 		console.log('log out');
+		renderApp();
 		history.push('/');
 	}
 });
