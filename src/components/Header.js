500ec7bb446b72f699b076c3bf9d5738fa0a4a78
Leo Vujanic <leonard.vujanic@gmail.com>
2018-11-11 17:04:39 +0100

    Connect logOut action with button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,20 @@
 import {NavLink} from "react-router-dom";
 import React from "react";
+import {connect} from 'react-redux';
+import {startLogOut} from "../actions/auth";
 
-const Header = () => (
+export const Header = ({startLogOut}) => (
 	<header>
 		<h1>Expensify</h1>
 		<NavLink to="/dashboard" activeClassName="is-active" exact={true}>Dashboard</NavLink>
 		<NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
 		<NavLink to="/help" activeClassName="is-active">Help</NavLink>
-		<button>Log Out</button>
+		<button onClick={startLogOut}>Log Out</button>
 	</header>
 );
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+	startLogOut: () => dispatch(startLogOut()),
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
