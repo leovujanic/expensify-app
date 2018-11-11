27997ca677bf238d45082208f58339bc46105aaf
Leo Vujanic <leonard.vujanic@gmail.com>
2018-11-11 17:10:38 +0100

    Add additional tests for login action calls

--- src/tests/components/LoginPage.test.js
+++ src/tests/components/LoginPage.test.js
@@ -2,7 +2,15 @@ import React from 'react';
 import {shallow} from 'enzyme';
 import {LoginPage} from '../../components/LoginPage';
 
+
 test('Should correctly render LoginPage', () => {
 	const wrapper = shallow(<LoginPage/>);
 	expect(wrapper).toMatchSnapshot();
+});
+
+test('Should call startLogout on button click', () => {
+	const startLogin = jest.fn();
+	const wrapper = shallow(<LoginPage startLogin={startLogin}/>);
+	wrapper.find('button').simulate('click');
+	expect(startLogin).toHaveBeenCalled();
 });
