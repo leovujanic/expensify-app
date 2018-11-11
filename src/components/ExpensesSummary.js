98e94287ecd9f6851511e3183a69812097727116
Leo Vujanic <leonard.vujanic@gmail.com>
2018-11-11 23:18:54 +0100

    Fix spacing between the words

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -14,9 +14,7 @@ export const ExpensesSummary = ({expenseCount, expensesTotal}) => {
 		<div className="page-header">
 			<div className="content-container">
 				<h1 className="page-header__title">
-					Viewing <span>{expenseCount}</span> {expenseWord}
-					totalling
-					<span>{formattedExpensesTotal}</span>
+					Viewing <span>{expenseCount}</span> {expenseWord} totalling <span>{formattedExpensesTotal}</span>
 				</h1>
 				<div className="page-header__actions">
 					<Link className="button" to="/create">Add Expense</Link>
